fddab22f8921375f1c951045af9c42d2440f52aa
Rado Kirov <rkirov@gmail.com>
2025-09-07 22:58:39 -0700

    more of 5.4

--- analysis/Analysis/Section_5_4.lean
+++ analysis/Analysis/Section_5_4.lean
@@ -82,18 +82,6 @@ theorem Real.isPos_def (x:Real) :
 theorem Real.isNeg_def (x:Real) :
     IsNeg x ↔ ∃ a:ℕ → ℚ, BoundedAwayNeg a ∧ (a:Sequence).IsCauchy ∧ x = LIM a := by rfl
 
--- turns out this was just abs_abs_sub_abs_le
-lemma abs_asb_sub(a b:ℚ): |(|a| - |b|)| ≤ |a - b| := by
-  rw [abs_sub_le_iff]
-  constructor
-  . simp
-    convert abs_add (a - b) b
-    ring_nf
-  . simp
-    rw [abs_sub_comm]
-    convert abs_add (b - a) a
-    ring_nf
-
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1 -/
 theorem Real.trichotomous (x:Real) : x = 0 ∨ x.IsPos ∨ x.IsNeg := by
   by_cases h: x = 0
@@ -112,26 +100,27 @@ theorem Real.trichotomous (x:Real) : x = 0 ∨ x.IsPos ∨ x.IsNeg := by
   simp [Section_4_3.dist] at ha
   specialize h N
   obtain ⟨j, hj, h⟩ := h
-  let a' := (fun n ↦ if n < N then ε / 2 else a n)
-  have ha'_equiv : Sequence.Equiv a a' := by
-    rw [Sequence.equiv_def]
-    intro ε' hε'
-    rw [Rat.eventuallyClose_iff]
-    use N
-    intro n hn
-    have hn': ¬ n < N := by linarith
-    unfold a'
-    simp [hn']
-    exact le_of_lt hε'
-
-  have ha' : (a':Sequence).IsCauchy := by
-    rw [← Sequence.isCauchy_of_equiv ha'_equiv]
-    exact ha_copy
-  specialize ha j hj
   by_cases hj': 0 ≤ a j
   . simp [abs_of_nonneg hj'] at h
     left
     rw [isPos_def]
+
+    let a' := (fun n ↦ if n < N then ε / 2 else a n)
+    have ha'_equiv : Sequence.Equiv a a' := by
+      rw [Sequence.equiv_def]
+      intro ε' hε'
+      rw [Rat.eventuallyClose_iff]
+      use N
+      intro n hn
+      have hn': ¬ n < N := by linarith
+      unfold a'
+      simp [hn']
+      exact le_of_lt hε'
+    have ha' : (a':Sequence).IsCauchy := by
+      rw [← Sequence.isCauchy_of_equiv ha'_equiv]
+      exact ha_copy
+    specialize ha j hj
+
     use a'
     constructor
     . rw [boundedAwayPos_def]
@@ -168,39 +157,299 @@ theorem Real.trichotomous (x:Real) : x = 0 ∨ x.IsPos ∨ x.IsNeg := by
       . exact ha'
       . apply (Real.LIM_eq_LIM ha_copy ha').mpr
         exact ha'_equiv
-  -- copy/paste
-  . sorry
+  -- some copy/paste by inequality proofs reworked
+  . simp at hj'
+    have h': a j < -ε := by
+      rw [abs_of_neg hj'] at h
+      linarith
+    right
+    rw [isNeg_def]
+    -- refactor with a theorem generalizing this construction
+    let a' := (fun n ↦ if n < N then -(ε / 2) else a n)
+    have ha'_equiv : Sequence.Equiv a a' := by
+      rw [Sequence.equiv_def]
+      intro ε' hε'
+      rw [Rat.eventuallyClose_iff]
+      use N
+      intro n hn
+      have hn': ¬ n < N := by linarith
+      unfold a'
+      simp [hn']
+      exact le_of_lt hε'
+    have ha' : (a':Sequence).IsCauchy := by
+      rw [← Sequence.isCauchy_of_equiv ha'_equiv]
+      exact ha_copy
+    specialize ha j hj
+
+    use a'
+    constructor
+    . rw [boundedAwayNeg_def]
+      use (ε / 2), (by positivity)
+      intro n
+      by_cases hn: n < N
+      . unfold a'
+        simp [hn]
+      . simp at hn
+        specialize ha n hn
+        suffices - (ε / 2) ≥ a n by
+          unfold a'
+          have : ¬ n < N := by linarith
+          simp [this]
+          linarith
+        by_cases hnj: a j ≤ a n
+        . have : |a j - a n| = a n - a j := by
+            rw [abs_sub_comm]
+            rw [abs_of_nonneg (by linarith)]
+          rw [this] at ha
+          calc
+            _ = -ε + (ε / 2) := by ring_nf
+            _ ≥ a j + (ε / 2) := by linarith
+            _ ≥ a j + a n - a j := by linarith
+            _ ≥ a n := by linarith
+        . apply le_of_lt
+          calc
+            - (ε / 2) ≥ - ε := by linarith
+            _ ≥ a j := by linarith
+            _ > a n := by
+              simp at hnj
+              exact hnj
+    . constructor
+      . exact ha'
+      . apply (Real.LIM_eq_LIM ha_copy ha').mpr
+        exact ha'_equiv
 
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1 -/
-theorem Real.not_zero_pos (x:Real) : ¬(x = 0 ∧ x.IsPos) := by sorry
+theorem Real.not_zero_pos (x:Real) : ¬(x = 0 ∧ x.IsPos) := by
+  push_neg
+  intro h
+  rw [isPos_def]
+  subst x
+  push_neg
+  intro a hb hc
+  symm
+  apply lim_of_boundedAwayZero
+  . exact BoundedAwayZero.boundedAwayPos hb
+  . exact hc
 
 theorem Real.nonzero_of_pos {x:Real} (hx: x.IsPos) : x ≠ 0 := by
   have := not_zero_pos x
   simpa [hx] using this
 
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1 -/
-theorem Real.not_zero_neg (x:Real) : ¬(x = 0 ∧ x.IsNeg) := by sorry
+theorem Real.not_zero_neg (x:Real) : ¬(x = 0 ∧ x.IsNeg) := by
+  push_neg
+  intro h
+  rw [isNeg_def]
+  subst x
+  push_neg
+  intro a hb hc
+  symm
+  apply lim_of_boundedAwayZero
+  . exact BoundedAwayZero.boundedAwayNeg hb
+  . exact hc
 
 theorem Real.nonzero_of_neg {x:Real} (hx: x.IsNeg) : x ≠ 0 := by
   have := not_zero_neg x
   simpa [hx] using this
 
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1 -/
-theorem Real.not_pos_neg (x:Real) : ¬(x.IsPos ∧ x.IsNeg) := by sorry
+theorem Real.not_pos_neg (x:Real) : ¬(x.IsPos ∧ x.IsNeg) := by
+  rw [isNeg_def, isPos_def]
+  push_neg
+  intro h
+  obtain ⟨ a, ha_pos, ha_cauchy, rfl ⟩ := h
+  intro a' ha' ha'c
+  intro hl
+  rw [LIM_eq_LIM ha_cauchy ha'c] at hl
+  rw [Sequence.equiv_def] at hl
+  rw [boundedAwayPos_def] at ha_pos
+  rw [boundedAwayNeg_def] at ha'
+  obtain ⟨ε, hε, hn⟩ := ha_pos
+  obtain ⟨ε', hε', hn'⟩ := ha'
+  specialize hl ε hε
+  rw [Rat.eventuallyClose_iff] at hl
+  obtain ⟨N, hl⟩ := hl
+  specialize hn N
+  specialize hn' N
+  specialize hl N (by rfl)
+  have : a N - a' N ≥ 0 := by
+    simp
+    apply le_of_lt
+    calc
+      _ ≤ -ε' := hn'
+      _ < 0 := by linarith
+      _ < ε := by linarith
+      _ ≤ a N := by linarith
+  rw [abs_of_nonneg this] at hl
+  linarith
+
+lemma BoundedAwayZero.boundedAwayNeg_of_neg_pos {a:ℕ → ℚ} (ha: BoundedAwayPos a) :
+  BoundedAwayNeg (fun n ↦ - a n) := by
+  rw [boundedAwayPos_def] at ha
+  rw [boundedAwayNeg_def]
+  obtain ⟨ c, hc, h ⟩ := ha
+  use c, hc
+  intro n
+  specialize h n
+  linarith
+
+
+lemma BoundedAwayZero.boundedAwayPos_of_neg_neg {a:ℕ → ℚ} (ha: BoundedAwayNeg a) :
+  BoundedAwayPos (fun n ↦ - a n) := by
+  -- same proof as above, how can it be refactored?
+  rw [boundedAwayNeg_def] at ha
+  rw [boundedAwayPos_def]
+  obtain ⟨ c, hc, h ⟩ := ha
+  use c, hc
+  intro n
+  specialize h n
+  linarith
+
+lemma Sequence.IsCauchy_neg {a : ℕ → ℚ} (ha : IsCauchy a) : IsCauchy (fun n ↦ - a n) := by
+  rw [Sequence.IsCauchy.coe] at ha ⊢
+  intro ε hε
+  obtain ⟨N, hN⟩ := ha ε hε
+  use N
+  intro j hj k hk
+  specialize hN j hj k hk
+  rw [Section_4_3.dist] at hN ⊢
+  rw [abs_sub_comm]
+  simp
+  rw [add_comm]
+  rw [_root_.sub_eq_add_neg] at hN
+  exact hN
 
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1 -/
 @[simp]
-theorem Real.neg_iff_pos_of_neg (x:Real) : x.IsNeg ↔ (-x).IsPos := by sorry
+theorem Real.neg_iff_pos_of_neg (x:Real) : x.IsNeg ↔ (-x).IsPos := by
+  constructor
+  . intro h
+    rw [isNeg_def] at h
+    rw [isPos_def]
+    obtain ⟨a, ha, ha', hlim⟩ := h
+    use fun n ↦ - a n
+    constructor
+    . exact BoundedAwayZero.boundedAwayPos_of_neg_neg ha
+    . constructor
+      . exact Sequence.IsCauchy_neg ha'
+      . subst x
+        rw [neg_LIM a ha']
+        rfl
+  . intro h
+    rw [isPos_def] at h
+    rw [isNeg_def]
+    obtain ⟨a, ha, ha', hlim⟩ := h
+    use fun n ↦ - a n
+    constructor
+    . exact BoundedAwayZero.boundedAwayNeg_of_neg_pos ha
+    . constructor
+      . exact Sequence.IsCauchy_neg ha'
+      . have : x = - LIM a := by
+          rw [← hlim]
+          ring
+        subst x
+        rw [neg_LIM a ha']
+        rfl
 
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1-/
-theorem Real.pos_add {x y:Real} (hx: x.IsPos) (hy: y.IsPos) : (x+y).IsPos := by sorry
+theorem Real.pos_add {x y:Real} (hx: x.IsPos) (hy: y.IsPos) : (x+y).IsPos := by
+  obtain ⟨a, ha, ha', hlim⟩ := hx
+  obtain ⟨b, hb, hb', hlim'⟩ := hy
+  use fun n ↦ a n + b n
+  constructor
+  . rw [boundedAwayPos_def] at ha hb ⊢
+    obtain ⟨c, hc, ha⟩ := ha
+    obtain ⟨d, hd, hb⟩ := hb
+    use c + d, by linarith
+    intro n
+    specialize ha n
+    specialize hb n
+    linarith
+  . constructor
+    . exact Sequence.IsCauchy.add  ha' hb'
+    . subst x
+      subst y
+      rw [LIM_add ha' hb']
+      rfl
 
 /-- Proposition 5.4.4 (basic properties of positive reals) / Exercise 5.4.1 -/
-theorem Real.pos_mul {x y:Real} (hx: x.IsPos) (hy: y.IsPos) : (x*y).IsPos := by sorry
+theorem Real.pos_mul {x y:Real} (hx: x.IsPos) (hy: y.IsPos) : (x*y).IsPos := by
+  obtain ⟨a, ha, ha', hlim⟩ := hx
+  obtain ⟨b, hb, hb', hlim'⟩ := hy
+  use fun n ↦ a n * b n
+  constructor
+  . rw [boundedAwayPos_def] at ha hb ⊢
+    obtain ⟨c, hc, ha⟩ := ha
+    obtain ⟨d, hd, hb⟩ := hb
+    use c * d, by positivity
+    intro n
+    specialize ha n
+    specialize hb n
+    exact mul_le_mul ha hb (by linarith) (by linarith)
+  . constructor
+    . exact Sequence.IsCauchy.mul ha' hb'
+    . subst x
+      subst y
+      rw [LIM_mul ha' hb']
+      rfl
 
-theorem Real.pos_of_coe (q:ℚ) : (q:Real).IsPos ↔ q > 0 := by sorry
+theorem Real.pos_of_coe (q:ℚ) : (q:Real).IsPos ↔ q > 0 := by
+  constructor
+  . intro h
+    rw [isPos_def] at h
+    obtain ⟨a, ha, ha', hlim⟩ := h
+    rw [ratCast_def] at hlim
+    rw [LIM_eq_LIM (Sequence.IsCauchy.const q) ha'] at hlim
+    rw [boundedAwayPos_def] at ha
+    rw [Sequence.equiv_def] at hlim
+    obtain ⟨c, hc, ha⟩ := ha
+    specialize hlim (c / 2) (by positivity)
+    rw [Rat.eventuallyClose_iff] at hlim
+    obtain ⟨N, hlim⟩ := hlim
+    specialize hlim N (by linarith)
+    specialize ha N
+    have := sub_le_of_abs_sub_le_left hlim
+    linarith
+  . intro h
+    rw [isPos_def]
+    use (fun _:ℕ ↦ q)
+    constructor
+    . rw [boundedAwayPos_def]
+      use q, h
+      intro n
+      rfl
+    . constructor
+      . exact Sequence.IsCauchy.const q
+      . rw [ratCast_def]
 
-theorem Real.neg_of_coe (q:ℚ) : (q:Real).IsNeg ↔ q < 0 := by sorry
+theorem Real.neg_of_coe (q:ℚ) : (q:Real).IsNeg ↔ q < 0 := by
+  constructor
+  . intro h
+    rw [isNeg_def] at h
+    obtain ⟨a, ha, ha', hlim⟩ := h
+    rw [ratCast_def] at hlim
+    rw [LIM_eq_LIM (Sequence.IsCauchy.const q) ha'] at hlim
+    rw [boundedAwayNeg_def] at ha
+    rw [Sequence.equiv_def] at hlim
+    obtain ⟨c, hc, ha⟩ := ha
+    specialize hlim (c / 2) (by positivity)
+    rw [Rat.eventuallyClose_iff] at hlim
+    obtain ⟨N, hlim⟩ := hlim
+    specialize hlim N (by linarith)
+    specialize ha N
+    have := sub_le_of_abs_sub_le_right hlim
+    linarith
+  . intro h
+    rw [isNeg_def]
+    use (fun _:ℕ ↦ q)
+    constructor
+    . rw [boundedAwayNeg_def]
+      use -q, (by linarith)
+      intro n
+      simp
+    . constructor
+      . exact Sequence.IsCauchy.const q
+      . rw [ratCast_def]
 
 open Classical in
 /-- Need to use classical logic here because isPos and isNeg are not decidable -/

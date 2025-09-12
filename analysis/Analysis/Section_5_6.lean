1644d50906ef25fa9aebef07cbb13a115776447e
Rado Kirov <rkirov@gmail.com>
2025-09-12 00:18:47 -0700

    add 5.6.1

--- analysis/Analysis/Section_5_6.lean
+++ analysis/Analysis/Section_5_6.lean
@@ -41,31 +41,153 @@ to be a `Field`), but the spirit of the exercises is to adapt the proofs of
 Proposition 4.3.10 that you previously established. -/
 
 /-- Analogue of Proposition 4.3.10(a) -/
-theorem Real.pow_add (x:Real) (m n:ℕ) : x^n * x^m = x^(n+m) := by sorry
+theorem Real.pow_add (x:Real) (m n:ℕ) : x^n * x^m = x^(n+m) := by
+  induction' n with n ih
+  . rw [pow_zero]
+    rw [one_mul]
+    rw [_root_.zero_add]
+  . rw [pow_succ]
+    rw [mul_comm _ x]
+    rw [mul_assoc]
+    rw [ih]
+    rw [mul_comm x]
+    rw [← pow_succ]
+    rw [_root_.add_assoc]
+    rw [add_comm _ 1]
+    rw [← _root_.add_assoc]
 
 /-- Analogue of Proposition 4.3.10(a) -/
-theorem Real.pow_mul (x:Real) (m n:ℕ) : (x^n)^m = x^(n*m) := by sorry
+theorem Real.pow_mul (x:Real) (m n:ℕ) : (x^n)^m = x^(n*m) := by
+  induction' m with m ih
+  . rw [pow_zero]
+    rw [mul_zero]
+    rw [pow_zero]
+  . rw [pow_succ]
+    rw [ih]
+    rw [pow_add]
+    rw [mul_add]
+    rw [mul_one]
 
 /-- Analogue of Proposition 4.3.10(a) -/
-theorem Real.mul_pow (x y:Real) (n:ℕ) : (x*y)^n = x^n * y^n := by sorry
+theorem Real.mul_pow (x y:Real) (n:ℕ) : (x*y)^n = x^n * y^n := by
+  induction' n with n ih
+  . repeat rw [pow_zero]
+    rw [mul_one]
+  . rw [pow_succ]
+    rw [ih]
+    have : x ^ n * y ^ n * (x * y) = x ^ n * x ^ 1 * y ^ n * y ^ 1 := by
+      repeat rw [pow_one]
+      ring
+    rw [this]
+    rw [pow_add]
+    rw [mul_assoc]
+    rw [pow_add]
 
 /-- Analogue of Proposition 4.3.10(b) -/
-theorem Real.pow_eq_zero (x:Real) (n:ℕ) (hn : 0 < n) : x^n = 0 ↔ x = 0 := by sorry
+theorem Real.pow_eq_zero (x:Real) (n:ℕ) (hn : 0 < n) : x^n = 0 ↔ x = 0 := by
+  constructor
+  . intro h
+    induction' n with n ih
+    . contradiction
+    . rw [pow_succ] at h
+      by_cases hz : n = 0
+      . rw [hz] at h
+        rw [pow_zero] at h
+        rw [one_mul] at h
+        exact h
+      . have : 0 < n := by exact Nat.zero_lt_of_ne_zero hz
+        specialize ih this
+        have h' : x ^ n = 0 ∨ x = 0 := by exact mul_eq_zero.mp h
+        cases' h' with h h
+        . exact ih h
+        . exact h
+  . intro h
+    rw [h]
+    induction' n with n ih
+    . contradiction
+    . rw [pow_succ]
+      by_cases hz: n = 0
+      . rw [hz]
+        ring
+      . have : 0 < n := by exact Nat.zero_lt_of_ne_zero hz
+        specialize ih this
+        rw [ih]
+        rw [mul_zero]
 
 /-- Analogue of Proposition 4.3.10(c) -/
-theorem Real.pow_nonneg {x:Real} (n:ℕ) (hx: x ≥ 0) : x^n ≥ 0 := by sorry
+theorem Real.pow_nonneg {x:Real} (n:ℕ) (hx: x ≥ 0) : x^n ≥ 0 := by
+  induction' n with n ih
+  . rw [pow_zero]
+    norm_num
+  . rw [pow_succ]
+    exact mul_nonneg ih hx
 
 /-- Analogue of Proposition 4.3.10(c) -/
-theorem Real.pow_pos {x:Real} (n:ℕ) (hx: x > 0) : x^n > 0 := by sorry
+theorem Real.pow_pos {x:Real} (n:ℕ) (hx: x > 0) : x^n > 0 := by
+  have hx': x ≥ 0 := by exact le_of_lt hx
+  have := pow_nonneg n hx'
+  rw [ge_iff_le] at this
+  rw [gt_iff_lt]
+  rw [le_iff_lt_or_eq] at this
+  cases' this with h h
+  . exact h
+  . by_cases hn : n = 0
+    . subst n
+      rw [pow_zero] at h
+      linarith
+    . have hn' : 0 < n := by exact Nat.zero_lt_of_ne_zero hn
+      have := (pow_eq_zero x _ hn').mp h.symm
+      subst x
+      linarith
 
 /-- Analogue of Proposition 4.3.10(c) -/
-theorem Real.pow_ge_pow (x y:Real) (n:ℕ) (hxy: x ≥ y) (hy: y ≥ 0) : x^n ≥ y^n := by sorry
+theorem Real.pow_ge_pow (x y:Real) (n:ℕ) (hxy: x ≥ y) (hy: y ≥ 0) : x^n ≥ y^n := by
+  induction' n with n ih
+  . rw [pow_zero]
+    rw [pow_zero]
+  . rw [pow_succ]
+    rw [pow_succ]
+    apply mul_le_mul ih hxy hy
+    have hx : x ≥ 0 := by exact le_trans hy hxy
+    exact pow_nonneg n hx
 
 /-- Analogue of Proposition 4.3.10(c) -/
-theorem Real.pow_gt_pow (x y:Real) (n:ℕ) (hxy: x > y) (hy: y ≥ 0) (hn: n > 0) : x^n > y^n := by sorry
+theorem Real.pow_gt_pow (x y:Real) (n:ℕ) (hxy: x > y) (hy: y ≥ 0) (hn: n > 0) : x^n > y^n := by
+  induction' n with n ih
+  . contradiction
+  . rw [pow_succ]
+    rw [pow_succ]
+    by_cases hz: n = 0
+    . subst n
+      simp only [pow_zero, one_mul, gt_iff_lt]
+      exact hxy
+    . have hn' : 0 < n := by exact Nat.zero_lt_of_ne_zero hz
+      specialize ih hn'
+      have hx' : x ^ n > 0 := by
+        apply pow_pos
+        exact lt_of_le_of_lt hy hxy
+      by_cases hy': y = 0
+      . subst y
+        simp
+        exact Left.mul_pos hx' hxy
+      simp at hy
+      rw [le_iff_lt_or_eq] at hy
+      have : 0 ≠ y := by simp only [ne_eq]; contrapose! hy'; exact hy'.symm
+      simp [this] at hy
+      have hy' : y ^ n > 0 := by exact pow_pos n hy
+      exact mul_lt_mul_of_pos' ih hxy hy hx'
 
 /-- Analogue of Proposition 4.3.10(d) -/
-theorem Real.pow_abs (x:Real) (n:ℕ) : |x|^n = |x^n| := by sorry
+theorem Real.pow_abs (x:Real) (n:ℕ) : |x|^n = |x^n| := by
+  induction' n with n ih
+  . rw [pow_zero]
+    rw [pow_zero]
+    have : 0 < (1: Real) := by exact zero_lt_one' Real
+    rw [_root_.abs_of_nonneg (le_of_lt this)]
+  . rw [pow_succ]
+    rw [pow_succ]
+    rw [ih]
+    rw [abs_mul (x ^ n) x] -- what?
 
 /-- Definition 5.6.2 (Exponentiating a real by an integer). Here we use the Mathlib definition coming from `DivInvMonoid`. -/
 lemma Real.pow_eq_pow (x: Real) (n:ℕ): x ^ (n:ℤ) = x ^ n := by rfl

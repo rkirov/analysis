e0ffff4c2f0e41862a2512eea40c2ad8bbe4c901
Rado Kirov <rkirov@gmail.com>
2025-09-13 21:52:21 -0700

    finish root problems in 5.6

--- analysis/Analysis/Section_5_6.lean
+++ analysis/Analysis/Section_5_6.lean
@@ -415,44 +415,378 @@ theorem Real.rootset_bddAbove {x:Real} (n:ℕ) (hn: n ≥ 1) : BddAbove { y:Real
     exact pow_lt_pow_right₀ this hn
   linarith
 
+-- taken from baby Rudin page 10
+theorem pow_sub_pow {a b: Real} {n: ℕ} (hn: 1 < n) (ha: 0 ≤ a) (hb: a < b) :
+  b ^ n - a ^ n < (b - a) * n * (b ^ (n - 1)) := by sorry
+
 /-- Lemma 5.6.6 (ab) / Exercise 5.6.1 -/
 theorem Real.eq_root_iff_pow_eq {x y:Real} (hx: x ≥ 0) (hy: y ≥ 0) {n:ℕ} (hn: n ≥ 1) :
-  y = x.root n ↔ y^n = x := by sorry
+  y = x.root n ↔ y^n = x := by
+  unfold root
+  have hlub := ExtendedReal.sSup_of_bounded (Real.rootset_nonempty hx n hn)
+    (Real.rootset_bddAbove n hn)
+  rw [isLUB_def] at hlub
+  -- have to deal with x = 0 case separately
+  -- todo: see if this can be avioded
+  by_cases hn0: x = 0
+  . subst x
+    set S : Set Real := {y | y ≥ 0 ∧ y ^ n ≤ 0}
+    have h_eq(x: Real): S = ({0} : Set Real) := by
+      ext x
+      simp
+      constructor
+      . intro hx
+        simp at hx
+        obtain ⟨hx1, hx2⟩ := hx
+        have : x ^ n ≥ 0 := by exact pow_nonneg n hx1
+        have : x ^ n = 0 := by exact (le_antisymm hx2 this)
+        exact (pow_eq_zero x n hn).mp this
+      . intro h
+        subst x
+        unfold S
+        simp
+        right
+        exact zero_pow (by linarith)
+    simp_all
+    intro h
+    exact Nat.ne_zero_of_lt hn
+  replace hx: 0 < x := by
+    simp at hx
+    rw [le_iff_lt_or_eq] at hx
+    have : 0 ≠ x := by exact fun a ↦ hn0 (id (Eq.symm a))
+    simp [this] at hx
+    exact hx
+  -- have to deal with n = 0 case separately
+  -- todo: see if this can be avioded
+  by_cases hnone: n = 1
+  . subst n
+    simp_all
+    set S : Real := sSup {y | y ≥ 0 ∧ y ≤ x}
+    suffices h_eq: S = x by simp [h_eq]
+    obtain ⟨h1, h2⟩ := hlub
+    rw [Real.upperBound_def] at h1
+    specialize h1 x (by simp; linarith)
+    specialize h2 x (by rw [Real.upperBound_def]; simp)
+    linarith
+  replace hn: 1 < n := by
+    simp at hn
+    rw [le_iff_lt_or_eq] at hn
+    have : 1 ≠ n := by exact fun a ↦ hnone (id (Eq.symm a))
+    simp [this] at hn
+    exact hn
+  constructor
+  . intro h
+    rw [← h] at hlub
+    replace ⟨h1, h2⟩ := hlub
+    rw [Real.upperBound_def] at h1
+    rcases trichotomous' (y ^ n) x with (h | h | h)
+    . exfalso
+      let ε := (y ^ n - x) / (n * y ^ (n - 1))
+      have hy: 0 < y := by
+        simp at h
+        have := hx.trans h
+        simp at hy
+        rw [le_iff_lt_or_eq] at hy
+        cases' hy with hy hy
+        . exact hy
+        . exfalso
+          rw [← hy] at this
+          rw [zero_pow (by linarith)] at this
+          linarith
+      have hε : 0 < ε := by
+        unfold ε
+        apply div_pos
+        . linarith
+        . apply mul_pos
+          . positivity
+          . positivity
+      have hε' : ε < y := by
+        unfold ε
+        calc
+          _ = y ^ n / (n * y ^ (n - 1)) - x / (n * y ^ (n - 1)) := by
+            ring
+          _ < y ^ n / (n * y ^ (n - 1)) := by
+            simp
+            positivity
+          _ = y / n := by
+            field_simp
+            ring_nf
+            have : y * y ^ (n - 1) = y ^ n := by
+              conv_lhs =>
+                arg 1
+                rw [show y = y ^ 1 by simp]
+              rw [pow_add y (n - 1) 1]
+              congr
+              omega
+            rw [this]
+          _ ≤ y := by
+            exact div_nat_le_self_of_nonnneg hy.le n
+      specialize h2 (y - ε) (by
+        rw [Real.upperBound_def]
+        simp
+        intro t ht ht'
+        by_contra ht''
+        simp at ht''
+        have h: y ^ n - t ^ n < y ^ n - x :=
+          calc
+            _ ≤ y ^ n - (y - ε) ^ n := by
+              gcongr
+              linarith
+            _ < _ := by exact pow_sub_pow (a:=y - ε) (b:=y) hn (by linarith) (by linarith)
+            _ = ε * n * y ^ (n - 1) := by simp
+            _ = (y ^ n - x) / (n * y ^ (n - 1)) *  n * y ^ (n - 1) := by
+              unfold ε
+              rfl
+            _ = y ^ n - x := by
+              field_simp
+              ring_nf
+        simp at h
+        linarith
+      )
+      linarith
+    . exfalso
+      -- magic choice taken from baby Rudin page 10
+      let ε := min (1/2) ((x - y^n) / (2 * n * (y + 1)^(n - 1)))
+      have hε : 0 < ε := by
+        unfold ε
+        apply lt_min (by norm_num)
+        apply div_pos
+        . linarith
+        . positivity
+      have hε' : ε < 1 := by
+        unfold ε
+        calc
+          _ ≤ (1:Real)/2 := min_le_left _ _
+          _ < (1:Real) := by norm_num
+      have hε'' : ε < (x - y^n) / (n * (y + 1)^(n - 1)) := by
+        unfold ε
+        calc
+          _ ≤ (x - y^n) / (2 * n * (y + 1)^(n - 1)) := min_le_right _ _
+          _ < (x - y^n) / (n * (y + 1)^(n - 1)) := by
+            gcongr
+            . linarith
+            . norm_cast
+              exact lt_two_mul_self (by linarith)
+      specialize h1 (y + ε) (by
+        simp
+        constructor
+        . linarith
+        . have :=
+            calc
+              (y + ε) ^ n - y ^ n < _ := pow_sub_pow (a:=y) (b:= y + ε) hn (by linarith) (by linarith)
+              _ ≤ ε * n * (y + 1)^(n - 1) := by simp; gcongr
+              _ < (x - y^n) / (n * (y + 1)^(n - 1)) *  n * (y + 1)^(n - 1) := by gcongr
+              _ = x - y^n := by field_simp; ring
+          simp at this
+          exact this.le
+      )
+      linarith
+    . exact h
+  . intro h
+    subst x
+    simp_all
+    set X := sSup {y_1 | 0 ≤ y_1 ∧ y_1 ^ n ≤ y ^ n}
+    obtain ⟨h1, h2⟩ := hlub
+    rw [Real.upperBound_def] at h1
+    specialize h1 y (by simp; exact hy)
+    specialize h2 y (by
+      rw [Real.upperBound_def]
+      intro x hx
+      simp at hx
+      obtain ⟨hx1, hx2⟩ := hx
+      exact le_of_pow_le_pow_left₀ (by linarith) hy hx2
+    )
+    linarith
 
 /-- Lemma 5.6.6 (c) / Exercise 5.6.1 -/
-theorem Real.root_nonneg {x:Real} (hx: x ≥ 0) {n:ℕ} (hn: n ≥ 1) : x.root n ≥ 0 := by sorry
+theorem Real.root_nonneg {x:Real} (hx: x ≥ 0) {n:ℕ} (hn: n ≥ 1) : x.root n ≥ 0 := by
+  unfold root
+  have := ExtendedReal.sSup_of_bounded (Real.rootset_nonempty hx n hn)
+    (Real.rootset_bddAbove n hn)
+  rw [isLUB_def] at this
+  replace ⟨h1, h2⟩ := this
+  rw [Real.upperBound_def] at h1
+  set y := sSup {y | y ≥ 0 ∧ y ^ n ≤ x}
+  specialize h1 0 (by simp; rw [zero_pow (by linarith)]; exact hx)
+  exact h1
 
 /-- Lemma 5.6.6 (c) / Exercise 5.6.1 -/
-theorem Real.root_pos {x:Real} (hx: x ≥ 0) {n:ℕ} (hn: n ≥ 1) : x.root n > 0 ↔ x > 0 := by sorry
+theorem Real.root_pos {x:Real} (hx: x ≥ 0) {n:ℕ} (hn: n ≥ 1) : x.root n > 0 ↔ x > 0 := by
+  have := Real.eq_root_iff_pow_eq hx (Real.root_nonneg hx hn) hn
+  simp at this
+  have hy := (Real.root_nonneg hx hn)
+  set y := x.root n
+  rw [← this]
+  constructor
+  . intro h
+    exact pow_pos n h
+  . intro h
+    have hn' : n ≠ 0 := by exact Nat.ne_zero_of_lt hn
+    have : 0 = (0:Real)^n := by exact Eq.symm ((fun x n hn ↦ (pow_eq_zero x n hn).mpr) 0 n hn rfl)
+    rw [this] at h
+    exact lt_of_pow_lt_pow_left₀ n hy h
 
 theorem Real.pow_of_root {x:Real} (hx: x ≥ 0) {n:ℕ} (hn: n ≥ 1) :
-  (x.root n)^n = x := by sorry
+  (x.root n)^n = x := by
+  have := Real.eq_root_iff_pow_eq hx (Real.root_nonneg hx hn) hn
+  simp at this
+  exact this
 
 theorem Real.root_of_pow {x:Real} (hx: x ≥ 0) {n:ℕ} (hn: n ≥ 1) :
-  (x^n).root n = x := by sorry
+  (x^n).root n = x := by
+  have hx' : x ^ n ≥ 0 := by exact pow_nonneg n hx
+  have hn' : n ≠ 0 := by exact Nat.ne_zero_of_lt hn
+  have := Real.eq_root_iff_pow_eq hx' (Real.root_nonneg hx' hn) hn
+  simp at this
+  rw [pow_left_inj₀ (root_nonneg hx' hn) hx hn'] at this
+  exact this
 
 /-- Lemma 5.6.6 (d) / Exercise 5.6.1 -/
-theorem Real.root_mono {x y:Real} (hx: x ≥ 0) (hy: y ≥ 0) {n:ℕ} (hn: n ≥ 1) : x > y ↔ x.root n > y.root n := by sorry
+theorem Real.root_mono {x y:Real} (hx: x ≥ 0) (hy: y ≥ 0) {n:ℕ} (hn: n ≥ 1) : x > y ↔ x.root n > y.root n := by
+  have h1 := Real.eq_root_iff_pow_eq hx (Real.root_nonneg hx hn) hn
+  simp at h1
+  have hxr := (Real.root_nonneg hx hn)
+  set xr := x.root n
+  rw [← h1]
+  have h2 := Real.eq_root_iff_pow_eq hy (Real.root_nonneg hy hn) hn
+  simp at h2
+  have hyr := (Real.root_nonneg hy hn)
+  set yr := y.root n
+  rw [← h2]
+  constructor
+  . intro h
+    exact lt_of_pow_lt_pow_left₀ n hxr h
+  . intro h
+    exact pow_gt_pow xr yr n h hyr hn
 
 /-- Lemma 5.6.6 (e) / Exercise 5.6.1 -/
-theorem Real.root_mono_of_gt_one {x : Real} (hx: x > 1) {k l: ℕ} (hkl: k > l) (hl: l ≥ 1) : x.root k < x.root l := by sorry
+theorem Real.root_mono_of_gt_one {x : Real} (hx: x > 1) {k l: ℕ} (hkl: k > l) (hl: l ≥ 1) : x.root k < x.root l := by
+  have h1 := Real.eq_root_iff_pow_eq (x:=x) (n:=l) (by linarith) (Real.root_nonneg (by linarith) (by linarith)) (by linarith)
+  simp at h1
+  set xl := x.root l
+  have h2 := Real.eq_root_iff_pow_eq (x:=x) (n:=k) (by linarith) (Real.root_nonneg (x:=x) (n:=k) (by linarith) (by linarith)) (by linarith)
+  simp at h2
+  set xk := x.root k
+  rw [← h1] at h2
+  have hxl: xl > 1 := by
+    have hxrl: xl ^ l > 1 := by
+      rw [h1]
+      exact hx
+    rw [show (1:Real) = 1^l by simp] at hxrl
+    exact lt_of_pow_lt_pow_left₀ l (Real.root_nonneg (by linarith) (by linarith)) hxrl
+  have hxk: xk > 1 := by
+    have hxrk: xk ^ k > 1 := by
+      rw [h2]
+      exact lt_of_lt_of_eq hx (id (Eq.symm h1))
+    rw [show (1:Real) = 1^k by simp] at hxrk
+    exact lt_of_pow_lt_pow_left₀ k (Real.root_nonneg (by linarith) (by linarith)) hxrk
+  rcases trichotomous' xl xk with (h | h | h)
+  . exact h
+  . exfalso
+    have :=
+      calc
+        xl ^ k < xk ^ k := by exact pow_gt_pow xk xl k h (by linarith) (by linarith)
+        _ = xl ^ l := by rw [h2]
+    have : xl ^ l < xl ^ k := by exact pow_lt_pow_right₀ hxl hkl
+    linarith
+  . exfalso
+    rw [h] at h2
+    have : k = l := by exact (pow_right_inj₀ (by linarith) (by linarith)).mp h2
+    linarith
 
 /-- Lemma 5.6.6 (e) / Exercise 5.6.1 -/
-theorem Real.root_mono_of_lt_one {x : Real} (hx0: 0 < x) (hx: x < 1) {k l: ℕ} (hkl: k > l) (hl: l ≥ 1) : x.root k > x.root l := by sorry
+theorem Real.root_mono_of_lt_one {x : Real} (hx0: 0 < x) (hx: x < 1) {k l: ℕ} (hkl: k > l) (hl: l ≥ 1) : x.root k > x.root l := by
+  have h1 := Real.eq_root_iff_pow_eq (x:=x) (n:=l) (by linarith) (Real.root_nonneg (by linarith) (by linarith)) (by linarith)
+  simp at h1
+  set xl := x.root l
+  have h2 := Real.eq_root_iff_pow_eq (x:=x) (n:=k) (by linarith) (Real.root_nonneg (x:=x) (n:=k) (by linarith) (by linarith)) (by linarith)
+  simp at h2
+  set xk := x.root k
+  rw [← h1] at h2
+  have hxl: xl < 1 := by
+    have hxrl: xl ^ l < 1 := by
+      rw [h1]
+      exact hx
+    rw [show (1:Real) = 1^l by simp] at hxrl
+    exact lt_of_pow_lt_pow_left₀ l (by norm_num) hxrl
+  have hxk: xk < 1 := by
+    have hxrk: xk ^ k < 1 := by
+      rw [h2]
+      exact lt_of_eq_of_lt h1 hx
+    rw [show (1:Real) = 1^k by simp] at hxrk
+    exact lt_of_pow_lt_pow_left₀ k (by norm_num) hxrk
+  have hxl': 0 < xl := by
+    unfold xl
+    exact (Real.root_pos (by linarith) (by linarith)).mpr hx0
+  have hxk': 0 < xk := by
+    unfold xk
+    exact (Real.root_pos (by linarith) (by linarith)).mpr hx0
+  rcases trichotomous' xl xk with (h | h | h)
+  . exfalso
+    have :=
+      calc
+        xl ^ k > xk ^ k := by exact pow_gt_pow xl xk k h (by linarith) (by linarith)
+        _ = xl ^ l := by rw [h2]
+    have : xl ^ l > xl ^ k := by exact pow_lt_pow_right_of_lt_one₀ hxl' hxl hkl
+    linarith
+  . exact h
+  . exfalso
+    rw [h] at h2
+    have : k = l := by exact (pow_right_inj₀ (by linarith) (by linarith)).mp h2
+    linarith
 
 /-- Lemma 5.6.6 (e) / Exercise 5.6.1 -/
-theorem Real.root_of_one {k: ℕ} (hk: k ≥ 1): (1:Real).root k = 1 := by sorry
+theorem Real.root_of_one {k: ℕ} (hk: k ≥ 1) (hk: k ≥ 1): (1:Real).root k = 1 := by
+  have h1 := Real.eq_root_iff_pow_eq (x:=1) (y:=1) (n:=k) (by norm_num) (by norm_num) hk
+  simp at h1
+  rw [← h1]
 
 /-- Lemma 5.6.6 (f) / Exercise 5.6.1 -/
-theorem Real.root_mul {x y:Real} (hx: x ≥ 0) (hy: y ≥ 0) {n:ℕ} (hn: n ≥ 1) : (x*y).root n = (x.root n) * (y.root n) := by sorry
+theorem Real.root_mul {x y:Real} (hx: x ≥ 0) (hy: y ≥ 0) {n:ℕ} (hn: n ≥ 1) : (x*y).root n = (x.root n) * (y.root n) := by
+  have hx': x.root n ≥ 0 := by exact Real.root_nonneg hx hn
+  have hy': y.root n ≥ 0 := by exact Real.root_nonneg hy hn
+  have h1 := Real.eq_root_iff_pow_eq (x:=x*y) (y:= (x.root n) * (y.root n)) (n:=n)
+    (by exact Left.mul_nonneg hx hy) (by exact Left.mul_nonneg hx' hy') hn
+  simp at h1
+  symm
+  rw [h1]
+  rw [Real.mul_pow]
+  rw [Real.pow_of_root hx hn]
+  rw [Real.pow_of_root hy hn]
 
-/-- Lemma 5.6.6 (g) / Exercise 5.6.1 -/
-theorem Real.root_root {x:Real} (hx: x ≥ 0) {n m:ℕ} (hn: n ≥ 1) (hm: m ≥ 1): (x.root n).root m = x.root (n*m) := by sorry
+/-- Lemma 5.6.6 (f) / Exercise 5.6.1 -/
+theorem Real.root_root {x:Real} (hx: x ≥ 0) {n m:ℕ} (hn: n ≥ 1) (hm: m ≥ 1): (x.root n).root m = x.root (n*m) := by
+  have hmn : n * m ≥ 1 := by exact Right.one_le_mul hn hm
+  have hmn': x.root (n * m) ≥ 0 := by exact Real.root_nonneg hx hmn
+  have h1 := Real.eq_root_iff_pow_eq hx (hmn') hmn
+  simp at h1
+  rw [mul_comm] at h1
+  rw [← pow_mul] at h1
+  conv_lhs => rw [← h1]
+  rw [Real.root_of_pow (by
+    apply pow_nonneg
+    . rw [mul_comm] at hmn
+      exact Real.root_nonneg hx hmn
+    ) hn]
+  rw [Real.root_of_pow (by
+    rw [mul_comm] at hmn'
+    exact hmn'
+  ) hm]
+  rw [mul_comm]
 
-theorem Real.root_one {x:Real} (hx: x > 0): x.root 1 = x := by sorry
+theorem Real.root_one {x:Real} (hx: x > 0): x.root 1 = x := by
+  have hn := Real.root_nonneg (x:=x) (n:=1) (by linarith) (by norm_num)
+  have h1 := Real.eq_root_iff_pow_eq (x:=x) (n:=1) (by linarith) hn (by norm_num)
+  simp at h1
+  exact h1
 
 theorem Real.pow_cancel {y z:Real} (hy: y > 0) (hz: z > 0) {n:ℕ} (hn: n ≥ 1)
-  (h: y^n = z^n) : y = z := by sorry
+  (h: y^n = z^n) : y = z := by
+  have := congrArg (root . n) h
+  simp at this
+  rw [Real.root_of_pow (by linarith) hn] at this
+  rw [Real.root_of_pow (by linarith) hn] at this
+  exact this
 
 example : ¬(∀ (y:Real) (z:Real) (n:ℕ) (_: n ≥ 1) (_: y^n = z^n), y = z) := by
   simp; refine ⟨ (-3), 3, 2, ?_, ?_, ?_ ⟩ <;> norm_num

232e9e4baee25c675ce8be3502c442497e6ed98c
Rado Kirov <rkirov@gmail.com>
2025-11-01 22:59:07 -0700

    clean up defs

--- analysis/Analysis/Section_6_1.lean
+++ analysis/Analysis/Section_6_1.lean
@@ -1387,15 +1387,16 @@ namespace Chapter6
 theorem Chapter5.Sequence.equiv_rat (a b: ℕ → ℚ) :
   Chapter5.Sequence.Equiv a b ↔ Chapter5.Sequence.RatEquiv a b := by sorry
 -- additional definitions for exercise 6.1.10
-abbrev _root_.Real.SeqCloseSeq (ε: ℝ) (a b: Chapter5.Sequence) : Prop :=
+abbrev Real.SeqCloseSeq (ε: ℝ) (a b: Chapter5.Sequence) : Prop :=
   ∀ n, n ≥ a.n₀ → n ≥ b.n₀ → ε.Close (a n) (b n)
 
-abbrev _root_.Real.SeqEventuallyClose (ε: ℝ) (a b: Chapter5.Sequence): Prop :=
+abbrev Real.SeqEventuallyClose (ε: ℝ) (a b: Chapter5.Sequence): Prop :=
   ∃ N, ε.SeqCloseSeq (a.from N) (b.from N)
 
 abbrev Chapter5.Sequence.RatEquiv (a b: ℕ → ℚ) : Prop :=
   ∀ (ε:ℝ), ε > 0 → ε.SeqEventuallyClose  (a:Chapter5.Sequence) (b:Chapter5.Sequence)
 
+namespace Chapter6
 theorem Chapter5.Sequence.equiv_rat (a b: ℕ → ℚ) :
     Chapter5.Sequence.Equiv a b ↔ Chapter5.Sequence.RatEquiv a b := by
   constructor

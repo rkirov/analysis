be71566bcd72ea5b3ef6857030d956ba2c72a385
Rado Kirov <rkirov@gmail.com>
2025-11-08 13:26:00 -0800

    Part of 6.2

--- analysis/Analysis/Section_6_2.lean
+++ analysis/Analysis/Section_6_2.lean
@@ -75,25 +75,128 @@ example : ¬ (3:EReal) ≤ ⊥ := by
 #check instCompleteLinearOrderEReal
 
 /-- Proposition 6.2.5(a) / Exercise 6.2.1 -/
-theorem EReal.refl (x:EReal) : x ≤ x := by sorry
+theorem EReal.refl (x:EReal) : x ≤ x := by rfl
 
 /-- Proposition 6.2.5(b) / Exercise 6.2.1 -/
-theorem EReal.trichotomy (x y:EReal) : x < y ∨ x = y ∨ x > y := by sorry
+theorem EReal.trichotomy (x y:EReal) : x < y ∨ x = y ∨ x > y := by
+  by_cases h : x.IsFinite
+  . obtain ⟨ x', rfl ⟩ := h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      simp only [EReal.coe_lt_coe_iff, EReal.coe_eq_coe_iff, gt_iff_lt]
+      exact lt_trichotomy x' y'
+    . rw [← infinite_iff_not_finite] at h'
+      obtain rfl | rfl := h' <;> simp
+  . rw [← infinite_iff_not_finite] at h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      obtain rfl | rfl := h <;> simp
+    . rw [← infinite_iff_not_finite] at h'
+      obtain rfl | rfl := h <;> obtain rfl | rfl := h' <;> simp
 
 /-- Proposition 6.2.5(b) / Exercise 6.2.1 -/
-theorem EReal.not_lt_and_eq (x y:EReal) : ¬ (x < y ∧ x = y) := by sorry
+theorem EReal.not_lt_and_eq (x y:EReal) : ¬ (x < y ∧ x = y) := by
+  push_neg
+  by_cases h : x.IsFinite
+  . obtain ⟨ x', rfl ⟩ := h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      simp only [EReal.coe_lt_coe_iff, ne_eq, EReal.coe_eq_coe_iff]
+      intro h
+      exact ne_of_lt h
+    . rw [← infinite_iff_not_finite] at h'
+      obtain rfl | rfl := h' <;> simp
+  . rw [← infinite_iff_not_finite] at h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      obtain rfl | rfl := h <;> simp
+    . rw [← infinite_iff_not_finite] at h'
+      intro hlt
+      obtain rfl | rfl := h <;> obtain rfl | rfl := h' <;> simp at hlt
+      exact Ne.symm infty_neq_neg_infty
 
 /-- Proposition 6.2.5(b) / Exercise 6.2.1 -/
-theorem EReal.not_gt_and_eq (x y:EReal) : ¬ (x > y ∧ x = y) := by sorry
+theorem EReal.not_gt_and_eq (x y:EReal) : ¬ (x > y ∧ x = y) := by
+  push_neg
+  by_cases h : x.IsFinite
+  . obtain ⟨ x', rfl ⟩ := h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      simp only [EReal.coe_lt_coe_iff, ne_eq, EReal.coe_eq_coe_iff]
+      intro h
+      exact ne_of_gt h
+    . rw [← infinite_iff_not_finite] at h'
+      obtain rfl | rfl := h' <;> simp
+  . rw [← infinite_iff_not_finite] at h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      obtain rfl | rfl := h <;> simp
+    . rw [← infinite_iff_not_finite] at h'
+      intro hlt
+      obtain rfl | rfl := h <;> obtain rfl | rfl := h' <;> simp at hlt
+      exact infty_neq_neg_infty
 
 /-- Proposition 6.2.5(b) / Exercise 6.2.1 -/
-theorem EReal.not_lt_and_gt (x y:EReal) : ¬ (x < y ∧ x > y) := by sorry
+theorem EReal.not_lt_and_gt (x y:EReal) : ¬ (x < y ∧ x > y) := by
+  push_neg
+  by_cases h : x.IsFinite
+  . obtain ⟨ x', rfl ⟩ := h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      simp only [EReal.coe_lt_coe_iff, EReal.coe_le_coe_iff]
+      intro h
+      exact le_of_lt h
+    . rw [← infinite_iff_not_finite] at h'
+      obtain rfl | rfl := h' <;> simp
+  . rw [← infinite_iff_not_finite] at h
+    by_cases h' : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := h'
+      obtain rfl | rfl := h <;> simp
+    . rw [← infinite_iff_not_finite] at h'
+      intro hlt
+      obtain rfl | rfl := h <;> obtain rfl | rfl := h' <;> simp at hlt
+      exact OrderBot.bot_le ⊤
 
 /-- Proposition 6.2.5(c) / Exercise 6.2.1 -/
-theorem EReal.trans {x y z:EReal} (hxy : x ≤ y) (hyz: y ≤ z) : x ≤ z := by sorry
+theorem EReal.trans {x y z:EReal} (hxy : x ≤ y) (hyz: y ≤ z) : x ≤ z := by
+  by_cases hx : x.IsFinite
+  . obtain ⟨ x', rfl ⟩ := hx
+    by_cases hy : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := hy
+      by_cases hz : z.IsFinite
+      . obtain ⟨ z', rfl ⟩ := hz
+        simp_all
+        exact Std.le_trans hxy hyz
+      . rw [← infinite_iff_not_finite] at hz
+        obtain rfl | rfl := hz <;> simp at hyz
+        exact OrderTop.le_top _
+    . rw [← infinite_iff_not_finite] at hy
+      obtain rfl | rfl := hy <;> simp at hxy
+      have : z = ⊤ := by exact eq_top_iff.mpr hyz
+      subst z
+      exact OrderTop.le_top _
+  . rw [← infinite_iff_not_finite] at hx
+    obtain rfl | rfl := hx <;> simp at hxy
+    . subst y
+      exact hyz
+    . exact OrderBot.bot_le z
 
 /-- Proposition 6.2.5(d) / Exercise 6.2.1 -/
-theorem EReal.neg_of_lt {x y:EReal} (hxy : x ≤ y): -y ≤ -x := by sorry
+theorem EReal.neg_of_lt {x y:EReal} (hxy : x ≤ y): -y ≤ -x := by
+  by_cases hx : x.IsFinite
+  . obtain ⟨ x', rfl ⟩ := hx
+    by_cases hy : y.IsFinite
+    . obtain ⟨ y', rfl ⟩ := hy
+      simp at hxy ⊢
+      exact hxy
+    . rw [← infinite_iff_not_finite] at hy
+      obtain rfl | rfl := hy <;> simp at hxy
+      exact OrderBot.bot_le _
+  . rw [← infinite_iff_not_finite] at hx
+    obtain rfl | rfl := hx <;> simp at hxy
+    . subst y
+      rfl
+    . simp only [neg_bot, le_top]
 
 /-- Definition 6.2.6 -/
 theorem EReal.sup_of_bounded_nonempty {E: Set ℝ} (hbound: BddAbove E) (hnon: E.Nonempty) :
@@ -135,12 +238,70 @@ theorem EReal.inf_eq_neg_sup (E: Set EReal) : sInf E = - sSup (-E) := by
   . intro h a ha; specialize h (-a) (by simp [ha]); grind [neg_le_neg_iff]
   grind [EReal.le_neg_of_le_neg]
 
+theorem EReal.inf_of_infty_mem {E: Set EReal} : sInf E = sInf (E \ {⊤}) := by
+  exact Eq.symm (sInf_diff_singleton_top E)
+
 /-- Example 6.2.7 -/
-abbrev Example_6_2_7 : Set EReal := { x | ∃ n:ℕ, x = -((n+1):EReal)} ∪ {⊥}
+abbrev Example_6_2_7 : Set EReal := { x | ∃ n:ℕ, x = -((n+1):Real)} ∪ {⊥}
+
+abbrev Example_6_2_7' : Set ℝ := { x | ∃ n:ℕ, x = -((n+1):ℝ)}
+
+theorem Example_6_2_7'_sup : sSup Example_6_2_7' = -1 := by
+  sorry
+
+theorem Example_6_2_7'_bounded : BddAbove Example_6_2_7' := by
+  use 0
+  intro x hx
+  obtain ⟨ n, rfl ⟩ := hx
+  simp
+  linarith
+
+theorem Example_6_2_7'_nonempty : Example_6_2_7'.Nonempty := by
+  use -1
+  simp
+
+theorem Example_6_2_7_eq : Example_6_2_7 \ {⊥} = (fun (x:Real) ↦ (x:EReal)) '' Example_6_2_7' := by
+  ext x
+  simp [Example_6_2_7, Example_6_2_7']
+  constructor
+  . intro h
+    obtain ⟨ h1, h2 ⟩ := h
+    obtain ⟨ n, rfl ⟩ := h1
+    simp at h2
+    use - n - 1
+    simp
+    constructor
+    . use n
+      grind
+    . rw [neg_add]
+      . right
+        norm_cast
+      . right
+        norm_cast
+  . intro h
+    obtain ⟨ y, hy1, hy2 ⟩ := h
+    obtain ⟨ n, rfl ⟩ := hy1
+    constructor
+    . use n
+      rw [← hy2]
+      simp
+      rw [neg_add]
+      . rw [add_comm]
+        rfl
+      . right
+        norm_cast
+      . right
+        norm_cast
+    . rw [← hy2]
+      exact real_neq_neg_infty _
 
 example : sSup Example_6_2_7 = -1 := by
   rw [EReal.sup_of_neg_infty_mem]
-  sorry
+  rw [show (-1:EReal) = (-1:ℝ) by rfl]
+  rw [Example_6_2_7_eq, ← Example_6_2_7'_sup]
+  apply EReal.sup_of_bounded_nonempty
+  . exact Example_6_2_7'_bounded
+  . exact Example_6_2_7'_nonempty
 
 example : sInf Example_6_2_7 = ⊥ := by
   rw [EReal.inf_eq_neg_sup]
@@ -160,12 +321,15 @@ example : sInf Example_6_2_9 = 1 := by sorry
 
 example : sSup Example_6_2_9 = ⊤ := by sorry
 
-example : sInf (∅ : Set EReal) = ⊤ := by sorry
+example : sInf (∅ : Set EReal) = ⊤ := by
+  rw [inf_eq_neg_sup]
+  simp only [Set.neg_empty, sSup_empty, neg_bot]
 
 example (E: Set EReal) : sSup E < sInf E ↔ E = ∅ := by sorry
 
 /-- Theorem 6.2.11 (a) / Exercise 6.2.2 -/
-theorem EReal.mem_le_sup (E: Set EReal) {x:EReal} (hx: x ∈ E) : x ≤ sSup E := by sorry
+theorem EReal.mem_le_sup (E: Set EReal) {x:EReal} (hx: x ∈ E) : x ≤ sSup E := by
+  sorry
 
 /-- Theorem 6.2.11 (a) / Exercise 6.2.2 -/
 theorem EReal.mem_ge_inf (E: Set EReal) {x:EReal} (hx: x ∈ E) : sInf E ≤ x := by sorry
@@ -186,14 +350,22 @@ noncomputable abbrev Chapter5.ExtendedReal.toEReal (x:ExtendedReal) : EReal := m
   | infty => ⊤
   | neg_infty => ⊥
 
-theorem Chapter5.ExtendedReal.coe_inj : Function.Injective toEReal := by sorry
+theorem Chapter5.ExtendedReal.coe_inj : Function.Injective toEReal := by
+  intro x y h
+  cases x <;> cases y <;> simp [toEReal] at h ⊢
+  . exact Real.equivR.injective h
 
-theorem Chapter5.ExtendedReal.coe_surj : Function.Surjective toEReal := by sorry
+theorem Chapter5.ExtendedReal.coe_surj : Function.Surjective toEReal := by
+  intro y
+  obtain ⟨ r, hr ⟩ | rfl | rfl := EReal.def y
+  . have := Real.equivR.surjective r
+    obtain ⟨ x, hx ⟩ := this
+    use ExtendedReal.real x
+    simp [toEReal, hx]
+    exact hr
+  . use ExtendedReal.infty
+  . use ExtendedReal.neg_infty
 
-noncomputable abbrev Chapter5.ExtendedReal.equivEReal : Chapter5.ExtendedReal ≃ EReal where
-  toFun := toEReal
-  invFun := sorry
-  left_inv x := by
-    sorry
-  right_inv x := by
-    sorry
+noncomputable abbrev Chapter5.ExtendedReal.equivEReal : Chapter5.ExtendedReal ≃ EReal :=
+  Equiv.ofBijective toEReal
+    ⟨ Chapter5.ExtendedReal.coe_inj, Chapter5.ExtendedReal.coe_surj ⟩
